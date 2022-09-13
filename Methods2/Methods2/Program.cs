d238142150b52a3d32f33cf66b1458e2aae2d7d0
DDon71 <theofficial1tap@gmail.com>
2022-09-13 10:38:11 -0400

    Methods Sheet

---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 1
+            // Name: Nicholas
+            // Favorite Colour: Red
+            // Favorite Animal: Great White Shark
+            // Favorite Artist: Youngboy
+
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. - What is your favorite colour?");
+            var colour = Console.ReadLine();
+
+            Console.WriteLine($"{colour} is an awesome colour! What is your first animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Oh I don't like that one! Now what is your favorite musical artist");
+            var Artist = Console.ReadLine();
+
+        }
+    }
+}
+
